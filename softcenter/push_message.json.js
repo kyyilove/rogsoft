2a65b2fa9d3c2d711c0e808f1085ce898641c236
sadoneli <sadoneli@gmail.com>
2018-12-04 19:49:18 +0800

    update pushmessage

--- softcenter/push_message.json.js
+++ softcenter/push_message.json.js
@@ -1,7 +1,8 @@
 {
 "title":"<em>软件中心&nbsp;-&nbsp;by&nbsp;koolshare</em>",
 "content1":"1.3.8更新日志：修复css样式",
-"content2":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
+"content2":"由于新版本固件移除了一些图像元素，导致一些插件会显示裂图，近期将会把所有插件更新一次，以修复此问题。——2018年12月4日",
+"content3":"<font color='#1E90FF'>交流反馈:&nbsp;&nbsp;</font>\
 <a href='https://github.com/koolshare/rogsoft' target='_blank'><em>1.软件中心GitHub项目</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='https://t.me/xbchat' target='_blank'><em>2.加入telegram群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
 <a href='http://shang.qq.com/wpa/qunwpa?idkey=f475468129ba8019245425559b5df5bdad7d7201ac7780417dd0218bbb4e1322' target='_blank'><em>3.加入QQ群</em></a>&nbsp;&nbsp;&nbsp;&nbsp;\
